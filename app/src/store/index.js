88760879169ffe937d3e3e90d3209c51fea3e57f
Jaime <jaimeloeuf@gmail.com>
2022-05-17 09:27:49 +0800

    Fix notification banner auto close bug.

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -7,19 +7,31 @@ export const useStore = defineStore("main", {
   state: () => ({
     notif: false,
     notifContent: "",
+    autoCloseTimeout: undefined,
   }),
 
-  // https://pinia.vuejs.org/core-concepts/actions.html
   actions: {
+    clearTimeoutIfAny() {
+      // If there is already an auto close timeout set, remove it,
+      // So that the auto close will not close a newly requested banner
+      if (this.autoCloseTimeout) {
+        clearTimeout(this.autoCloseTimeout);
+        this.autoCloseTimeout = undefined;
+      }
+    },
+
     showNotif(notifContent, timeout = 3000) {
+      this.clearTimeoutIfAny();
+
       this.notif = true;
       this.notifContent = notifContent;
 
       // Auto close notif banner after timeout
-      setTimeout(() => (this.notif = false), timeout);
+      this.autoCloseTimeout = setTimeout(() => (this.notif = false), timeout);
     },
 
     clearNotif() {
+      this.clearTimeoutIfAny();
       this.notif = false;
     },
   },
