8ff18641279d23ff1b210c71263e8868900db865
Jaime <jaimeloeuf@gmail.com>
2022-06-13 23:37:29 +0800

    WIP Refactor slugRedirect module.

--- redirect/src/slugRedirect.js
+++ redirect/src/slugRedirect.js
@@ -1,32 +1,22 @@
 /**
- * Get mapping from firestore and returns document snapshot if found
- * @param {String} slug Slug string
- * @param {String} host Host string, where this should come from req.get("host")
- * @returns {Promise<undefined | FirebaseFirestore.DocumentSnapshot<FirebaseFirestore.DocumentData>>} Returns either a document snapshot or undefined
+ * API logic to handle main slug to URL redirect requests
  */
-const getMapping = (slug, host) =>
-  require("@enkeldigital/firebase-admin")
+module.exports = async (slug, hostname, res) => {
+  // This should not happen as the base case / is caught by the base URL handler
+  // if (!slug) return res.status(500).send("Internal Error: Missing URL Slug!");
+
+  // Get mapping from firestore and returns document snapshot if found
+  const snapshot = await require("@enkeldigital/firebase-admin")
     .firestore()
     .collection("map")
     // Filter by slug first as it will narrow the results down much faster first compared to host
     // As a slug will probably be more unique than a hostname
     .where("slug", "==", slug)
-    .where("host", "==", host)
+    .where("host", "==", hostname)
     .get()
     .then((snapshot) => (snapshot.empty ? undefined : snapshot.docs[0]));
 
-/**
- * API logic to handle main slug to URL redirect requests
- */
-module.exports = async (req, res) => {
-  const { slug } = req.params;
-
-  // This should not happen as the base case / is caught by the base URL handler
-  // if (!slug) return res.status(500).send("Internal Error: Missing URL Slug!");
-
-  const snapshot = await getMapping(slug, req.hostname);
-
-  // @todo Potentially redirect to a statically hosted not found page instead!
+  // @todo Redirect to a statically hosted not found page instead!
   if (!snapshot) return res.status(404).send("Error: Invalid link");
 
   const { status, url } = snapshot.data();
@@ -38,6 +28,7 @@ module.exports = async (req, res) => {
 
   // Only track the API call after responding to the client to reduce client call latency
   // No need to await here as it is a fire and forget type
+  // @todo Here it is updating it in the map collection, but we still need to update again in the usage ...
   require("@enkeldigital/firebase-admin")
     .firestore()
     .collection("map")
