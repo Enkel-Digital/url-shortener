68b4bb918bcf835093a163639e3b2b0a3812c5d7
Jaime <jaimeloeuf@gmail.com>
2022-06-14 20:55:07 +0800

    Cleanup and add async error handler to redirect.

--- redirect/src/index.js
+++ redirect/src/index.js
@@ -20,45 +20,47 @@ const getSlug = (path) =>
   path.at(-1) === "/" && path.length > 1 ? path.slice(0, -1) : path;
 
 /** Main route handler for URL redirect requests */
-app.get("*", async (req, res) => {
-  // Get the slug string and remove the first slash
-  const slug = getSlug(req.path).slice(1);
+app.get(
+  "*",
+  asyncWrap(async (req, res) => {
+    // Get the slug string and remove the first slash
+    const slug = getSlug(req.path).slice(1);
 
-  // This should not happen as the base case / is caught by the base URL handler
-  // if (!slug) return res.status(500).send("Internal Error: Missing URL Slug!");
+    // Get mapping from firestore and returns document snapshot if found
+    const snapshot = await require("@enkeldigital/firebase-admin")
+      .firestore()
+      .collection("map")
+      // Filter by slug first as it will narrow the results down much faster first compared to host
+      // As a slug will probably be more unique than a hostname
+      .where("slug", "==", slug)
+      .where("host", "==", req.hostname)
+      .get()
+      .then((snapshot) => (snapshot.empty ? undefined : snapshot.docs[0]));
 
-  // Get mapping from firestore and returns document snapshot if found
-  const snapshot = await require("@enkeldigital/firebase-admin")
-    .firestore()
-    .collection("map")
-    // Filter by slug first as it will narrow the results down much faster first compared to host
-    // As a slug will probably be more unique than a hostname
-    .where("slug", "==", slug)
-    .where("host", "==", req.hostname)
-    .get()
-    .then((snapshot) => (snapshot.empty ? undefined : snapshot.docs[0]));
+    // @todo Redirect to a statically hosted not found page instead!
+    if (!snapshot) return res.status(404).send("Error: Invalid link");
 
-  // @todo Redirect to a statically hosted not found page instead!
-  if (!snapshot) return res.status(404).send("Error: Invalid link");
+    const { status, url } = snapshot.data();
+    if (!url)
+      return res
+        .status(500)
+        .send("Internal Error: Missing URL Slug in document");
 
-  const { status, url } = snapshot.data();
-  if (!url)
-    return res.status(500).send("Internal Error: Missing URL Slug in document");
+    // Respond with a redirect
+    res.redirect(status, url);
 
-  // Respond with a redirect
-  res.redirect(status, url);
-
-  // Only track the API call after responding to the client to reduce client call latency
-  // No need to await here as it is a fire and forget type
-  // @todo Here it is updating it in the map collection, but we still need to update again in the usage ...
-  require("@enkeldigital/firebase-admin")
-    .firestore()
-    .collection("map")
-    .doc(snapshot.id)
-    .update({
-      used: require("firebase-admin").firestore.FieldValue.increment(1),
-    });
-});
+    // Only track the API call after responding to the client to reduce client call latency
+    // No need to await here as it is a fire and forget type
+    // @todo Here it is updating it in the map collection, but we still need to update again in the usage ...
+    require("@enkeldigital/firebase-admin")
+      .firestore()
+      .collection("map")
+      .doc(snapshot.id)
+      .update({
+        used: require("firebase-admin").firestore.FieldValue.increment(1),
+      });
+  })
+);
 
 // Mount the 404 and 500 error handling middleware last
 // @todo Might use a custom 404 handler to give client more information, can be a route in the domain's landing page to tell them 404
