94be01b7860cc06b8c86bf619aafb8f2f94cf1a5
Jaime <jaimeloeuf@gmail.com>
2022-06-14 20:51:28 +0800

    Merge slugRedirect module into main:     Remove the seperate module to speed up parsing on cold starts.

--- redirect/src/index.js
+++ redirect/src/index.js
@@ -5,31 +5,68 @@ require("dotenv").config();
 const app = require("express")();
 const { asyncWrap, _404, _500 } = require("express-error-middlewares");
 
-app.use(require("cors")({ origin: "*" }));
+app
+  // Use * wildcard for CORS as all domains will be relying on the same server
+  .use(require("cors")({ origin: "*" }))
 
-// middleware to add http headers
-app.use(require("helmet")());
+  // middleware to add http headers
+  .use(require("helmet")())
 
-// Health probe to check if server is up without running any other logic
-app.get("/health", (req, res) => res.status(200).send("URL Shortener"));
+  // Health probe to check if server is up without running any other logic
+  // This is the only path that cannot be used as a slug
+  .get("/__health__", (_, res) => res.status(200).send("URL Shortener"));
 
-// Depending on whether a root redirect URL is provided, mount a different root API handler
-// Navigating to the base URL will either be a 301 redirect to the root company domain
-// Or a simple message to let user know what is this domain for
-if (process.env.root_redirect)
-  // Using 301 permanent redirect to ensure browsers cache this to prevent invoking server repeatedly to save cost
-  app.get("/", (req, res) => res.redirect(301, process.env.root_redirect));
-else app.get("/", (req, res) => res.status(200).send("URL Shortener"));
+const getSlug = (path) =>
+  path.at(-1) === "/" && path.length > 1 ? path.slice(0, -1) : path;
 
-app.get("/:slug", asyncWrap(require("./slugRedirect.js")));
+/** Main route handler for URL redirect requests */
+app.get("*", async (req, res) => {
+  // Get the slug string and remove the first slash
+  const slug = getSlug(req.path).slice(1);
+
+  // This should not happen as the base case / is caught by the base URL handler
+  // if (!slug) return res.status(500).send("Internal Error: Missing URL Slug!");
+
+  // Get mapping from firestore and returns document snapshot if found
+  const snapshot = await require("@enkeldigital/firebase-admin")
+    .firestore()
+    .collection("map")
+    // Filter by slug first as it will narrow the results down much faster first compared to host
+    // As a slug will probably be more unique than a hostname
+    .where("slug", "==", slug)
+    .where("host", "==", req.hostname)
+    .get()
+    .then((snapshot) => (snapshot.empty ? undefined : snapshot.docs[0]));
+
+  // @todo Redirect to a statically hosted not found page instead!
+  if (!snapshot) return res.status(404).send("Error: Invalid link");
+
+  const { status, url } = snapshot.data();
+  if (!url)
+    return res.status(500).send("Internal Error: Missing URL Slug in document");
+
+  // Respond with a redirect
+  res.redirect(status, url);
+
+  // Only track the API call after responding to the client to reduce client call latency
+  // No need to await here as it is a fire and forget type
+  // @todo Here it is updating it in the map collection, but we still need to update again in the usage ...
+  require("@enkeldigital/firebase-admin")
+    .firestore()
+    .collection("map")
+    .doc(snapshot.id)
+    .update({
+      used: require("firebase-admin").firestore.FieldValue.increment(1),
+    });
+});
 
 // Mount the 404 and 500 error handling middleware last
-// @todo Might use a custom 404 handler to give client more information
+// @todo Might use a custom 404 handler to give client more information, can be a route in the domain's landing page to tell them 404
 app.use(_404);
 app.use(_500);
 
 /**
  * @notice Setup PORT last to ensure all setup is done before server starts listening to traffic
  */
-const port = process.env.PORT || 3000; // Defaults to PORT 3000
+const port = process.env.PORT || 5000; // Defaults to PORT 5000
 app.listen(port, () => console.log(`Server running on port: ${port}`));
