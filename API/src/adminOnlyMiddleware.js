456071c597ecd5bff75675f5a200b1a629543018
Jaime <jaimeloeuf@gmail.com>
2022-06-25 22:42:30 +0800

    Rewrote adminOnlyMiddleware: 1.  Removed the firebase auth express middleware library as the way it     was being used before was very convoluted and inefficient where the     middleware was doing extra work by making a network call to get the     decoded token again using the UID when the token is already available     for use directly. 2.  Copied and refactor the code from the library and merged with the     custom admin only middleware logic to create a single middleware that     checks for both authn and admin authz.

--- API/src/adminOnlyMiddleware.js
+++ API/src/adminOnlyMiddleware.js
@@ -1,26 +1,33 @@
-/**
- * Authorization middleware
- * Using "Firebase Auth" for authentication
- */
-
 const firebaseAdmin = require("@enkeldigital/firebase-admin");
 
-// Admin only middleware is the combination of authentication middleware plus authorization middleware
-module.exports = [
-  require("firebase-auth-express-middleware")(firebaseAdmin),
+/**
+ * Middleware to ensure only admins with the admin claim on their JWT can pass through.
+ * Business logics need to handle extra conditions locally. E.g. user can only request for their own data.
+ */
+module.exports = async function auth(req, res, next) {
+  try {
+    // Get auth token if available and if it follows the "bearer" pattern
+    // @notice Headers are all lowercased by express
+    // https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_the_firebase_admin_sdk
+    // The verifyIdToken needs a project ID, but should be taken care of if firebase admin has been initialised properly or runs on gcp infra
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      req.authenticatedUser = await firebaseAdmin
+        .auth()
+        .verifyIdToken(req.headers.authorization.split(" ")[1]);
 
-  /**
-   * Only allow admins with the admin claim on their JWT to pass
-   * HTTP 403 means identity known but denied / failed authentication
-   */
-  async (req, res, next) =>
-    firebaseAdmin
-      .auth()
-      .getUser(req.authenticatedUser.uid)
-      .then((user) =>
-        user.customClaims?.admin === true
-          ? next()
-          : res.status(403).json({ error: "UNAUTHORIZED" })
-      )
-      .catch((e) => res.status(403).json({ error: e.message })),
-];
+      // Only allow request to continue down stream if user is an admin
+      return req.authenticatedUser.admin === true
+        ? next()
+        : res.status(403).json({ error: "UNAUTHORIZED" });
+    }
+
+    // 401 Missing auth token thus unauthorised
+    res.status(401).json({ error: "MISSING AUTH" });
+  } catch (error) {
+    // 403 identity known but denied / failed authentication
+    res.status(403).json({ error: "UNAUTHORIZED" });
+  }
+};
